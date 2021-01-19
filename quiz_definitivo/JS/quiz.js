6704d699a2170f04aeb56a797db0a3da31abf5b1
VitorGeovani <71882193+VitorGeovani@users.noreply.github.com>
2021-01-19 15:57:02 -0300

    Add files via upload

---
+++ quiz_definitivo/JS/quiz.js
@@ -0,0 +1,263 @@
+//Todas as questões
+function questao1() {
+    let resposta1 = document.getElementById("Batman").checked
+    
+
+    if (resposta1 == true) {
+        document.getElementById("resultado1").textContent = "Parabéns, você acertou essa questão!"
+        document.getElementById("resultado1").style.color = "green"
+        document.getElementById("spanAtual").textContent = pontoAtual + "/12"
+        pontoAtual = pontoAtual + 1
+    }else{
+        document.getElementById("resultado1").textContent = "Que pena, você errou essa questão!"
+        document.getElementById("resultado1").style.color = "red"
+        pontoAtual -1
+    } 
+}
+
+function questao2() {
+    let resposta2 = document.getElementById("Asgard").checked
+    
+    if (resposta2 == true) {
+        document.getElementById("resultado2").textContent = "Parabéns, você acertou essa questão!"
+        document.getElementById("resultado2").style.color = "green"
+        document.getElementById("spanAtual2").textContent = pontoAtual + "/12"
+        pontoAtual = pontoAtual + 1
+    }else{
+        document.getElementById("resultado2").textContent = "Que pena, você errou essa questão!"
+        document.getElementById("resultado2").style.color = "red"
+        pontoAtual -1
+    }
+}
+
+function questao3() {
+    let resposta3 = document.getElementById("Vibranium").checked
+
+    if (resposta3 == true) {
+        document.getElementById("resultado3").textContent = "Parabéns, você acertou essa questão!"
+        document.getElementById("resultado3").style.color = "green"
+        document.getElementById("spanAtual3").textContent = pontoAtual + "/12"
+        pontoAtual = pontoAtual + 1
+    }else{
+        document.getElementById("resultado3").textContent = "Que pena, você errou essa questão!"
+        document.getElementById("resultado3").style.color = "red"
+        pontoAtual -1
+    }
+}
+
+function questao4() {
+    let resposta4 = document.getElementById("LexLuthor").checked
+
+    if (resposta4 == true) {
+        document.getElementById("resultado4").textContent = "Parabéns, você acertou essa questão!"
+        document.getElementById("resultado4").style.color = "green"
+        document.getElementById("spanAtual4").textContent = pontoAtual + "/12"
+        pontoAtual = pontoAtual + 1
+    }else{
+        document.getElementById("resultado4").textContent = "Que pena, você errou essa questão!"
+        document.getElementById("resultado4").style.color = "red"
+        pontoAtual -1
+    }
+}
+
+function questao5() {
+    let resposta5 = document.getElementById("Batman2").checked
+
+    if (resposta5 == true) {
+        document.getElementById("resultado5").textContent = "Parabéns, você acertou essa questão!"
+        document.getElementById("resultado5").style.color = "green"
+        document.getElementById("spanAtual5").textContent = pontoAtual + "/12"
+        pontoAtual = pontoAtual + 1
+    }else{
+        document.getElementById("resultado5").textContent = "Que pena, você errou essa questão!"
+        document.getElementById("resultado5").style.color = "red"
+        pontoAtual -1
+    }
+
+}
+function questao6() {
+    let resposta6 = document.getElementById("Feiticeira").checked
+
+    if (resposta6 == true) {
+        document.getElementById("resultado6").textContent = "Parabéns, você acertou essa questão!"
+        document.getElementById("resultado6").style.color = "green"
+        document.getElementById("spanAtual6").textContent = pontoAtual + "/12"
+        pontoAtual = pontoAtual + 1
+    }else{
+        document.getElementById("resultado6").textContent = "Que pena, você errou essa questão!"
+        document.getElementById("resultado6").style.color = "red"
+        pontoAtual -1
+    }
+}
+
+function questao7() {
+    let resposta7 = document.getElementById("John").checked
+
+    if (resposta7 == true) {
+        document.getElementById("resultado7").textContent = "Parabéns, você acertou essa questão!"
+        document.getElementById("resultado7").style.color = "green"
+        document.getElementById("spanAtual7").textContent = pontoAtual + "/12"
+        pontoAtual = pontoAtual + 1
+    }else{
+        document.getElementById("resultado7").textContent = "Que pena, você errou essa questão!"
+        document.getElementById("resultado7").style.color = "red"
+        pontoAtual -1
+    }
+}
+
+function questao8() {
+    let resposta8 = document.getElementById("guerra3").checked
+
+    if (resposta8 == true) {
+        document.getElementById("resultado8").textContent = "Parabéns, você acertou essa questão!"
+        document.getElementById("resultado8").style.color = "green"
+        document.getElementById("spanAtual8").textContent = pontoAtual + "/12"
+        pontoAtual = pontoAtual + 1
+    }else{
+        document.getElementById("resultado8").textContent = "Que pena, você errou essa questão!"
+        document.getElementById("resultado8").style.color = "red"
+        pontoAtual -1
+
+    }
+}
+
+function questao9() {
+    let resposta9 = document.getElementById("Wally").checked
+
+    if (resposta9 == true) {
+        document.getElementById("resultado9").textContent = "Parabéns, você acertou essa questão!"
+        document.getElementById("resultado9").style.color = "green"
+        document.getElementById("spanAtual9").textContent = pontoAtual + "/12"
+        pontoAtual = pontoAtual + 1
+    }else{
+        document.getElementById("resultado9").textContent = "Que pena, você errou essa questão!"
+        document.getElementById("resultado9").style.color = "red"
+        pontoAtual -1
+    }
+}
+
+function questao10() {
+    let resposta10 = document.getElementById("incredible").checked
+
+    if (resposta10 == true) {
+        document.getElementById("resultado10").textContent = "Parabéns, você acertou essa questão!"
+        document.getElementById("resultado10").style.color = "green"
+        document.getElementById("spanAtual10").textContent = pontoAtual + "/12"
+        pontoAtual = pontoAtual + 1
+    }else{
+        document.getElementById("resultado10").textContent = "Que pena, você errou essa questão!"
+        document.getElementById("resultado10").style.color = "red"
+        pontoAtual -1
+    }
+}
+
+function questao11() {
+    let resposta11 = document.getElementById("Coringa2").checked
+
+    if (resposta11 == true) {
+        document.getElementById("resultado11").textContent = "Parabéns, você acertou essa questão!"
+        document.getElementById("resultado11").style.color = "green"
+        document.getElementById("spanAtual11").textContent = pontoAtual + "/12"
+        pontoAtual = pontoAtual + 1
+    }else{
+        document.getElementById("resultado11").textContent = "Que pena, você errou essa questão!"
+        document.getElementById("resultado11").style.color = "red"
+        pontoAtual -1
+    }
+}
+
+function questao12() {
+    let resposta12 = document.getElementById("Jotunheim2").checked
+
+    if (resposta12 == true) {
+        document.getElementById("resultado12").textContent = "Parabéns, você acertou essa questão!"
+        document.getElementById("resultado12").style.color = "green"
+        document.getElementById("spanAtual12").textContent = pontoAtual + "/12"
+        pontoAtual = pontoAtual + 1
+    }else{
+        document.getElementById("resultado12").textContent = "Que pena, você errou essa questão!"
+        document.getElementById("resultado12").style.color = "red"
+        pontoAtual -1
+        
+    }
+}
+
+//Tentar novamente
+function refresh() {
+    window.location.reload()
+}
+
+// Functions
+function gravarPontuacao(){
+    // Criando objeto e adicionando no array ranking
+    var nick = prompt("Digite seu Nome: ");
+
+    let obj = { name: nick, points: pontoAtual };
+    ranking.unshift(obj)
+
+    // Chamando função para atualizar o ranking
+    exibirRanking();
+
+    // Salvando o ranking
+    salvarRanking();
+}
+
+function salvarRanking(){
+    localStorage.setItem("ranking", JSON.stringify(ranking))
+}
+
+function exibirRanking(){   
+    // Esta funcão é responsável por ordenar o array com base no atributo PONTOS do OBJETO
+    function compare( a, b ) {
+        if ( a.points < b.points ){
+          return -1;
+        }
+        if ( a.points > b.points ){
+          return 1;
+        }
+        return 0;
+    }
+    
+    // Usando a funcao acima para ordenar os valores e revertendo para deixar-ló de fato na ordem decrescente
+    ranking.sort(compare);
+    ranking.reverse();
+
+    
+    position = 1; // Váriavel que indica a posição do jogador, usada para controlar o máximo de usuários na tabela
+    for(obj in ranking) {
+        // Pegando a key e obtendo o objeto atráves do array ranking
+        obj = ranking[obj];
+
+        // Atualizando o ranking com os dados na tabela
+        document.getElementById("nick_" + position).innerHTML = obj.name;
+        document.getElementById("points_" + position).innerHTML = obj.points;
+
+        // Só temos 3 posições na tabela, esta estrutura de decisão não deixa passar de 3 valores para evitar erro.
+        if(position >= 3){
+            break;
+        }else{
+            position++;
+        }
+    }
+}
+
+function recuperarRanking(){
+    // Verificando se o ranking existe no localstorage
+    if(localStorage.getItem("ranking") == null){
+        return []; // Se não existir retorna apenas um array vazio
+    }else{
+        // Se existir, retorna o ranking
+        let ranking = JSON.parse(localStorage.getItem("ranking"))
+        return ranking;
+    }
+}
+
+
+//Sistema de Ranking
+var pontoAtual = 1
+var ranking = recuperarRanking();
+
+// Exibir ranking
+exibirRanking();
+
+
